5c93fbf0272cc625f098f1c466179d71c5036d38
Hang <1506620085@qq.com>
2024-05-16 22:24:53 +0800

    init

---
+++ generator-basic/src/main/java/com/tailored/generator/StaticGenerator.java
@@ -0,0 +1,16 @@
+package com.tailored.generator;
+
+import cn.hutool.core.io.FileUtil;
+
+import java.io.File;
+
+/**
+ * 静态文件生成器
+ */
+public class StaticGenerator {
+    public static void main(String[] args) {
+    }
+
+    public static void doGenerate() {
+    }
+}
