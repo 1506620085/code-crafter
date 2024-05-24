fcfd5c38a6bee039c55465453185a5efb91abed4
Hang <1506620085@qq.com>
2024-05-24 10:14:12 +0800

    动态文件与静态文件结合生成

---
+++ generator-basic/src/main/java/com/tailored/generator/MainGenerator.java
@@ -0,0 +1,30 @@
+package com.tailored.generator;
+
+import com.tailored.model.MainTemplateConfig;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainGenerator {
+
+    public static void main(String[] args) throws TemplateException, IOException {
+        MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
+        mainTemplateConfig.setAuthor("Hangzzz");
+        mainTemplateConfig.setLoop(false);
+        mainTemplateConfig.setOutputText("求和结果123：");
+        doGenerator(mainTemplateConfig);
+    }
+
+    public static void doGenerator(Object model) throws TemplateException, IOException {
+        String projectPath = System.getProperty("user.dir");
+        File parentFile  = new File(projectPath).getParentFile();
+        String inputPath = parentFile.getAbsolutePath() + File.separator + "generation-demo-projects/acm-template";
+        String outputPath = projectPath;
+        StaticGenerator.copyFilesByRecursive(inputPath,outputPath);
+        // 生成动态文件
+        String inputDynamicFilePath = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String outputDynamicFilePath = outputPath + File.separator + "acm-template/src/com/yupi/acm/MainTemplate.java";
+        DynamicGenerator.doGenerate(inputDynamicFilePath,outputDynamicFilePath,model);
+    }
+}
--- generator-basic/src/main/java/com/tailored/generator/StaticGenerator.java
+++ generator-basic/src/main/java/com/tailored/generator/StaticGenerator.java
@@ -13,15 +13,6 @@ import java.nio.file.StandardCopyOption;
  */
 public class StaticGenerator {
 
-    /**
-     * 拷贝文件（Hutool 实现，会将输入目录完整拷贝到输出目录下）
-     *
-     * @param inputPath
-     * @param outputPath
-     */
-    public static void copyFilesByHutool(String inputPath, String outputPath) {
-        FileUtil.copy(inputPath, outputPath, false);
-    }
 
     public static void main(String[] args) {
         // 获取整个项目的根路径
@@ -32,6 +23,16 @@ public class StaticGenerator {
         // 输出路径：直接输出到项目的根目录
         String outputPath = projectPath;
         copyFilesByHutool(inputPath, outputPath);
+    }
+
+    /**
+     * 拷贝文件（Hutool 实现，会将输入目录完整拷贝到输出目录下）
+     *
+     * @param inputPath
+     * @param outputPath
+     */
+    public static void copyFilesByHutool(String inputPath, String outputPath) {
+        FileUtil.copy(inputPath, outputPath, false);
     }
 
     /**
