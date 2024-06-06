a19c4400e5fb94fa31408b615e83854a31c91e34
Hang <1506620085@qq.com>
2024-06-06 22:59:49 +0800

    feat:创建生成器模块

---
+++ code-crafter-maker/src/main/java/com/crafter/model/MainTemplateConfig.java
@@ -0,0 +1,25 @@
+package com.crafter.model;
+
+import lombok.Data;
+
+/**
+ * 动态模板配置
+ */
+@Data
+public class MainTemplateConfig {
+
+    /**
+     * 是否生成循环
+     */
+    private boolean loop = false;
+
+    /**
+     * 作者注释
+     */
+    private String author = "Hangz";
+
+    /**
+     * 输出信息
+     */
+    private String outputText = "求和结果：";
+}
