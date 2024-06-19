2c2794c591109de5cbe8a636c1af1051c2b2fade
Hang <1506620085@qq.com>
2024-06-19 22:06:12 +0800

    feat: 将生成代码生成器的代码用模板方法模式进行规范化

--- code-crafter-maker/src/main/java/com/crafter/maker/Main.java
+++ code-crafter-maker/src/main/java/com/crafter/maker/Main.java
@@ -3,10 +3,16 @@ package com.crafter.maker;
 
 import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.json.JSONUtil;
+import com.crafter.maker.generator.main.MainGenerator;
 import com.crafter.maker.meta.Meta;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
+        MainGenerator mainGenerator = new MainGenerator();
+        mainGenerator.doGenerate();
         String metaJson = ResourceUtil.readUtf8Str("meta.json");
         Meta meta = JSONUtil.toBean(metaJson, Meta.class);
     }
